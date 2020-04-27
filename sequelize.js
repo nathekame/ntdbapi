815be523bc25ef3bd8e7173b1935c5880444c1a1
n.athekame <n.athekame@gmail.com>
2020-04-27 22:55:48 +0100

    added a automated control number generation

--- sequelize.js
+++ sequelize.js
@@ -3,7 +3,8 @@ const userModel = require('./models/user');
 const metaModel = require('./models/meta');
 const controlNumberModel = require('./models/controlNumber');
 
-
+var fs = require('fs');
+var csv = require('csv');
 
 //cloud connect 
 const ho = 'us-cdbr-iron-east-01.cleardb.net'
@@ -50,6 +51,56 @@ sequelize.authenticate()
  const User = userModel(sequelize, Sequelize);
  const Meta = metaModel(sequelize, Sequelize);
  const ControlNumber = controlNumberModel(sequelize, Sequelize);
+
+
+// extra code to add data to db at build time
+
+
+//var Device = require('./models').Device
+
+var input = fs.createReadStream('./csv/test.csv');
+var parser = csv.parse({
+  delimiter: ',',
+  columns: true
+})
+
+
+var transform = csv.transform(function(row) {
+  var resultObj = {
+    controlNumber: row['CODE']
+    // imei: row['IMEI#'],
+    // serial_number: row['S/N #'],
+    // meid: row['MEID#'],
+    // esn: row['ESN#'],
+    // mac_address: row['MAC#'],
+    // exfactory_date: row['Ex-Factory date'],
+    // po_number: row['PO#'],
+    // packaging_number: row['CN#'] ,
+    // sw_version: row['Software version'],
+    // fw_version: row['Hardware version'],
+    // hw_version: row['S/N #'].slice(2, 4)
+  }
+  ControlNumber.create(resultObj)
+    .then(function() {
+      console.log('Control Number Created')
+    })
+    .catch(function(err) {
+      console.log('Error encountered: ' + err)
+    })
+})
+
+input.pipe(parser).pipe(transform)
+
+
+
+
+
+
+
+
+
+
+
 
 
 sequelize.sync({ force: false })
