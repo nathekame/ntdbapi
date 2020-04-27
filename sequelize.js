0cba9781554712049fc26e98da4216666ed47a2b
n.athekame <n.athekame@gmail.com>
2020-04-27 21:13:31 +0100

    db connect commit

--- sequelize.js
+++ sequelize.js
@@ -5,19 +5,38 @@ const controlNumberModel = require('./models/controlNumber');
 
 
 
-const sequelize = new Sequelize('ntdb', 'pmauser', 'password', {
-  host: 'localhost',
+//cloud connect 
+const ho = 'mysql://bb53c866eb5faf:ba8ce82d@us-cdbr-iron-east-01.cleardb.net/heroku_f8a080c180a554a?reconnect=true'
+
+const sequelize = new Sequelize('ntdb', 'bb53c866eb5faf', 'ba8ce82d', {
+  host: ho,
   dialect: 'mysql',
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
 
 
   });
+
+  //local connect
+
+// const sequelize = new Sequelize('ntdb', 'pmauser', 'password', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   },
+
+
+//   });
 
 sequelize.authenticate()
 	  .then(() => {
